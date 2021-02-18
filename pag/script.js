bd53132cca281002ff5cf018242930a3644c5663
diego1510vgdt <diego1510vgdt@outlook.com>
2021-02-18 11:23:46 -0600

    Realización de los esquemas finales de práctica y tarea.  Sigue pendiente el desarrollo del apartado de autor y sus elementos

--- pag/script.js
+++ pag/script.js
@@ -30,3 +30,26 @@ window.onscroll = function(){
     document.getElementById("backbtn").style.transform = "scale(0)"
   }
 };
+
+const modal = document.querySelector(".modal");
+const previews = document.querySelectorAll(".image-gallery img");
+const original = document.querySelector(".fullsimg");
+const caption = document.querySelector(".caption");
+
+previews.forEach(preview => {
+  preview.addEventListener('click', () => {
+    modal.classList.add("open");
+    original.classList.add("open");
+    const originalSrc = preview.getAttribute("data-original");
+    original.src = `./images/prac1/${originalSrc}`
+    const altText = preview.alt;
+    caption.textContent = altText;
+  });
+});
+
+modal.addEventListener('click', (e) => {
+  if(e.target.classList.contains('modal')){
+    modal.classList.remove('open');
+    original.classList.remove("open");
+  }
+});
